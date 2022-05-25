a06b96094cb35cef2a531bc5390cb51036331dcf
Krishna Chaitanya Kommera <kommerakrishnachaitanya@gmail.com>
2022-05-25 14:13:32 +0530

    posted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {React ,  useEffect} from "react";
+import About from "./components/About";
+import Card from './components/Card'
+import Footer from "./components/Footer";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Aos from "aos";
+import 'aos/dist/aos.css';
+
 
 function App() {
+  useEffect(() => {
+
+    Aos.init({
+      once:true,
+    })
+    
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100 overflow-hidden">
+      <div data-aos="fade-down" data-aos-duration="4000" >
+      <Card />
+
+      </div>
+      <div data-aos="fade-up" data-aos-duration="4000" data-aos-delay="1500">
+      
+      <About />
+
+      </div>
+      <Skills />
+    <div data-aos="fade-down-left" data-aos-duration="4000" data-aos-delay="800">
+      <Contact />
+
+      </div>
+      <div data-aos="fade-up-right" data-aos-duration="4000" data-aos-delay="800">
+      <Footer />
+
+      </div>
+    
     </div>
   );
 }
